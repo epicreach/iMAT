3e02f63795e332f272659c98c2c46fbbc2c6f3da
Luqas <118449875+epicreach@users.noreply.github.com>
2023-04-27 11:15:54 +0200

    first real commit

---
+++ iMatApp/src/imat/MainViewController.java
@@ -0,0 +1,26 @@
+
+package imat;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    Label pathLabel;
+
+    IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
+
+    public void initialize(URL url, ResourceBundle rb) {
+
+        String iMatDirectory = iMatDataHandler.imatDirectory();
+
+        pathLabel.setText(iMatDirectory);
+    }
+
+}
