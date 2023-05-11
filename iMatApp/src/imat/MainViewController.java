a4ea2b6fb7024efe7bae5748875409c6f5c50f6a
Luqas <118449875+epicreach@users.noreply.github.com>
2023-05-11 12:15:11 +0200

    frontend update
    la till en fxml fil för kundvagn popup
    la till en metod för att den skulle popa upp (funkar ej)

--- iMatApp/src/imat/MainViewController.java
+++ iMatApp/src/imat/MainViewController.java
@@ -4,16 +4,27 @@ package imat;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.awt.event.ActionEvent;
+import java.awt.geom.RoundRectangle2D;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
 import javafx.scene.paint.Color;
@@ -37,28 +48,65 @@ public class MainViewController implements Initializable {
     @FXML
     Button nextItem;
 
+    @FXML
+    Button tidigarekopbutton;
+
+    @FXML
+    Button favoriterbutton;
+
+    @FXML
+    Button profilbutton;
+
+    @FXML
+    Button leveranstiderbutton;
+
+    @FXML
+    HBox categoryTemplate;
+
+    @FXML
+    Button jamforkopvagnbutton;
+
+    @FXML
+    Button tillkassanbutton;
+
     int currentIndex = 1;
 
     IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
+
+    @FXML
+    private void handleButtonClick(ActionEvent event) throws IOException {
+        Parent popup = FXMLLoader.load(getClass().getResource("kundvagn_popout.fxml"));
+        Scene popupScene = new Scene(popup, 400, 400);
+        Stage window = new Stage();
+        window.setScene(popupScene);
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.showAndWait();
+    }
 
     public void initialize(URL url, ResourceBundle rb) {
 
         String iMatDirectory = iMatDataHandler.imatDirectory();
         
-        List<Product> products = iMatDataHandler.getProducts();
+        
+
+        fontSetter();
+
+
+        /*List<Product> products = iMatDataHandler.getProducts();
         refresh_item(1,products);
 
+        categoryTemplate.setSpacing(50);
 
         //Template setOnAction-funktion
         nextItem.setOnAction(event -> {
             currentIndex = (currentIndex + 1) % products.size();
             refresh_item(currentIndex, products);
         });
-
+*/
         
     }
 
-    public void refresh_item(int num, List<Product> products){
+    /*public void refresh_item(int num, List<Product> products){
             //Hämtar in en produkt på ett index
             Product item = iMatDataHandler.getProduct(num);
             //Laddar in bilden
@@ -70,6 +118,17 @@ public class MainViewController implements Initializable {
             productName.setText(text);
 
         
+    }*/
+
+    public void fontSetter(){
+        Font font = Font.font("Verdana Pro Black", FontWeight.BOLD, 20);
+        tidigarekopbutton.setFont(font);
+        leveranstiderbutton.setFont(font);
+        favoriterbutton.setFont(font);
+        profilbutton.setFont(font);
+
     }
+
+   
 
 }
