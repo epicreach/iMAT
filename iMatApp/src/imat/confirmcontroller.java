0f527ab17538fadd35c1b5ffcf9356de196cbe63
DavidGidlof <117651607+Ladibo@users.noreply.github.com>
2023-05-25 11:18:55 +0200

    confirmfxml och controller

---
+++ iMatApp/src/imat/confirmcontroller.java
@@ -0,0 +1,54 @@
+package imat;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import se.chalmers.cse.dat216.project.Order;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+
+public class Confirmcontroller implements Initializable{
+    @FXML 
+    Label confirmdate;
+
+    @FXML  
+    Label confirmorder;
+
+    @FXML
+    Button tillbakaimat;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    @FXML
+    private void goBackiMat(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("imat_app.fxml"));
+            Parent mainViewRoot = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getTarget()).getScene().getWindow();
+            stage.setScene(new Scene(mainViewRoot));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+}
+}
