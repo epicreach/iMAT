bd3525cf392272a1c197a868d393ffe1ffaea15d
DavidGidlof <117651607+Ladibo@users.noreply.github.com>
2023-05-25 10:59:08 +0200

    confirmpage made

--- iMatApp/src/imat/LeveransController.java
+++ iMatApp/src/imat/LeveransController.java
@@ -49,4 +49,17 @@ public class LeveransController implements Initializable {
             e.printStackTrace();
         }
 }
+@FXML
+private void movetoconfirm(ActionEvent event) {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("confirmpage.fxml"));
+        Parent mainViewRoot = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getTarget()).getScene().getWindow();
+        stage.setScene(new Scene(mainViewRoot));
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
 }
